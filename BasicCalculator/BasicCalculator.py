380855223100183ecb43aefb9da000f6dc23b811
Jared <97537529+JDEND@users.noreply.github.com>
2024-03-26 12:06:16 -0400

    added memory to calculations

--- BasicCalculator/BasicCalculator.py
+++ BasicCalculator/BasicCalculator.py
@@ -6,6 +6,8 @@ window = Tk()
 
 expression = ""
 
+prevNum = 0
+
 #basic function, uses a switch statment based on the passed in operator
 #to do the basic calculation, returns this value back
 def basic_cal(numb1, numb2, operator):
@@ -35,20 +37,30 @@ def press(num, _event=None):
 #calculate total
 def calculate(_event=None):
 
-    try:
+    #try:
         global expression
+        global prevNum
 
         expression = re.split(r"(\+|\*|-|/|\^)", expression)
-
-        total = str(basic_cal(float(expression[0]), float(expression[2]), expression[1]))
+        print(expression)
+        
+        if(expression[0] == ''):
+            print("here")
+            expression = (prevNum, expression[1], expression[2])
+            print(expression)
+        
+        total = basic_cal(float(expression[0]), float(expression[2]),
+                          expression[1])
+        prevNum = total
+        total = str(total)
  
         equation.set(total)
  
         expression = ""
  
-    except:
-        equation.set(" error ")
-        expression = ""
+    #except:
+    #    equation.set(" error ")
+    #    expression = ""
     
 #clear display
 def clear():
@@ -65,7 +77,8 @@ if __name__ == "__main__":
     window.maxsize(2160,3840)
 
     equation = StringVar()
-    expression_field = Entry(window, textvariable=equation, font='100', state=DISABLED)
+    expression_field = Entry(window, textvariable=equation, font='100', 
+                             state=DISABLED)
     expression_field.grid(columnspan=4, ipadx=70, ipady=70)
 
     #reactive window
@@ -100,59 +113,77 @@ if __name__ == "__main__":
     window.bind('.', lambda event:press("."))
     window.bind('<Return>', calculate)
 
-    button1 = Button(window, text=' 1 ', fg='black', bg='dark gray',font='10', command=lambda: press(1), height=5, width=10)
+    button1 = Button(window, text=' 1 ', fg='black', bg='dark gray',font='10', 
+                     command=lambda: press(1), height=5, width=10)
     button1.grid(row=2, column=0) 
  
-    button2 = Button(window, text=' 2 ', fg='black', bg='dark gray',font='10',command=lambda: press(2), height=5, width=10)
+    button2 = Button(window, text=' 2 ', fg='black', bg='dark gray',font='10',
+                     command=lambda: press(2), height=5, width=10)
     button2.grid(row=2, column=1)
 
-    button3 = Button(window, text=' 3 ', fg='black', bg='dark gray',font='10',command=lambda: press(3), height=5, width=10)
+    button3 = Button(window, text=' 3 ', fg='black', bg='dark gray',font='10',
+                     command=lambda: press(3), height=5, width=10)
     button3.grid(row=2, column=2)
  
-    button4 = Button(window, text=' 4 ', fg='black', bg='dark gray',font='10', command=lambda: press(4), height=5, width=10)
+    button4 = Button(window, text=' 4 ', fg='black', bg='dark gray',font='10', 
+                     command=lambda: press(4), height=5, width=10)
     button4.grid(row=3, column=0)
  
-    button5 = Button(window, text=' 5 ', fg='black', bg='dark gray',font='10', command=lambda: press(5), height=5, width=10)
+    button5 = Button(window, text=' 5 ', fg='black', bg='dark gray',font='10', 
+                     command=lambda: press(5), height=5, width=10)
     button5.grid(row=3, column=1)
  
-    button6 = Button(window, text=' 6 ', fg='black', bg='dark gray',font='10',command=lambda: press(6), height=5, width=10)
+    button6 = Button(window, text=' 6 ', fg='black', bg='dark gray',font='10',
+                     command=lambda: press(6), height=5, width=10)
     button6.grid(row=3, column=2)
  
-    button7 = Button(window, text=' 7 ', fg='black', bg='dark gray',font='10', command=lambda: press(7), height=5, width=10)
+    button7 = Button(window, text=' 7 ', fg='black', bg='dark gray',font='10', 
+                     command=lambda: press(7), height=5, width=10)
     button7.grid(row=4, column=0)
  
-    button8 = Button(window, text=' 8 ', fg='black', bg='dark gray',font='10', command=lambda: press(8), height=5, width=10)
+    button8 = Button(window, text=' 8 ', fg='black', bg='dark gray',font='10', 
+                     command=lambda: press(8), height=5, width=10)
     button8.grid(row=4, column=1)
  
-    button9 = Button(window, text=' 9 ', fg='black', bg='dark gray',font='10',command=lambda: press(9), height=5, width=10)
+    button9 = Button(window, text=' 9 ', fg='black', bg='dark gray',font='10',
+                     command=lambda: press(9), height=5, width=10)
     button9.grid(row=4, column=2)
  
-    button0 = Button(window, text=' 0 ', fg='black', bg='dark gray',font='10',command=lambda: press(0), height=5, width=10)
+    button0 = Button(window, text=' 0 ', fg='black', bg='dark gray',font='10',
+                     command=lambda: press(0), height=5, width=10)
     button0.grid(row=5, column=0)
  
-    plus = Button(window, text=' + ', fg='black', bg='dark gray',font='10',command=lambda: press("+"), height=5, width=10)
+    plus = Button(window, text=' + ', fg='black', bg='dark gray',font='10',
+                  command=lambda: press("+"), height=5, width=10)
     plus.grid(row=2, column=3)
  
-    minus = Button(window, text=' - ', fg='black', bg='dark gray',font='10',command=lambda: press("-"), height=5, width=10)
+    minus = Button(window, text=' - ', fg='black', bg='dark gray',font='10',
+                   command=lambda: press("-"), height=5, width=10)
     minus.grid(row=3, column=3)
  
-    multiply = Button(window, text=' * ', fg='black', bg='dark gray',font='10',command=lambda: press("*"), height=5, width=10)
+    multiply = Button(window, text=' * ', fg='black', bg='dark gray',font='10',
+                      command=lambda: press("*"), height=5, width=10)
     multiply.grid(row=4, column=3)
  
-    divide = Button(window, text=' / ', fg='black', bg='dark gray',font='10',command=lambda: press("/"), height=5, width=10)
+    divide = Button(window, text=' / ', fg='black', bg='dark gray',font='10',
+                    command=lambda: press("/"), height=5, width=10)
     divide.grid(row=5, column=3)
  
-    equal = Button(window, text=' = ', fg='black', bg='dark gray',font='10',command=calculate, height=5, width=10)
+    equal = Button(window, text=' = ', fg='black', bg='dark gray',font='10',
+                   command=calculate, height=5, width=10)
     equal.grid(row=5, column=2)
 
 
-    power = Button(window, text=' ^ ', fg='black', bg='dark gray',font='10',command=lambda: press("^"), height=5, width=10)
+    power = Button(window, text=' ^ ', fg='black', bg='dark gray',font='10',
+                   command=lambda: press("^"), height=5, width=10)
     power.grid(row=6, column=1)
  
-    clearb = Button(window, text='Clear', fg='black', bg='dark gray',font='10', command=clear, height=5, width=10)
+    clearb = Button(window, text='Clear', fg='black', bg='dark gray',font='10', 
+                    command=clear, height=5, width=10)
     clearb.grid(row=5, column='1')
  
-    Decimal= Button(window, text='.', fg='black', bg='dark gray',font='10', command=lambda: press('.'), height=5, width=10)
+    Decimal= Button(window, text='.', fg='black', bg='dark gray',font='10', 
+                    command=lambda: press('.'), height=5, width=10)
     Decimal.grid(row=6, column=0)
 
     window.mainloop()
